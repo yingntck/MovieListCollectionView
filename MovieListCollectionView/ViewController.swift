535812aa0635e9bbbd59a59cb5487807bad0a750
Nanthicha K <nanthicha13@gmail.com>
2019-09-17 17:18:09 +0700

    Setup project

---
+++ MovieListCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieListCollectionView
+//
+//  Created by Nanthicha Kritveeranant on 17/9/2562 BE.
+//  Copyright © 2562 Nanthicha Kritveeranant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MovieListCollectionView' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MovieListCollectionView
+  pod 'Alamofire', '5.0.0-beta.6'
+  pod 'AlamofireImage', '4.0.0-beta.3'
+  
+end
