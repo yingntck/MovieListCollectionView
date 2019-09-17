55875b449254b33d6c29e306f471b4fc6b1dc12f
Nanthicha K <nanthicha13@gmail.com>
2019-09-17 17:19:37 +0700

    Main storyboard added

---
+++ MovieListCollectionView/Scene/Main/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  MovieListCollectionView
+//
+//  Created by Nanthicha Kritveeranant on 17/9/2562 BE.
+//  Copyright © 2562 Nanthicha Kritveeranant. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+  @IBOutlet weak var posterImageView: UIImageView!
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var ratingLabel: UILabel!
+  
+}
